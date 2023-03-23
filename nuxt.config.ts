c06f79a2f084d4747cad590db9881b50f7ce4c4c
Sontaya Rungsaard <sontaya16071997@gmail.com>
2023-03-24 00:07:54 +0700

    add test auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  runtimeConfig: {
+    app: {
+      spotify: {
+        clientId: process.env.CLIENT_ID || '',
+        clientSecret: process.env.CLIENT_SECRET || '',
+        clientUri: process.env.CLIENT_URI || '',
+      }
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,71 @@
 <script setup lang="ts">
+import { ofetch } from 'ofetch'
+import { Buffer } from 'buffer'
+
+// const client_id = '' // Your client id
+// const client_secret = '' // Your secret
+// const redirect_uri = '' // Your redirect uri
+// let token: string | undefined = undefined
+const config = useRuntimeConfig();
+
+function generateRandomString (length: number) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+async function login () {
+  console.log('Login', config.app)
+  const url = 'https://accounts.spotify.com/authorize?'
+
+  const res = await ofetch(url, {
+    method: 'GET',
+    params: {
+      response_type: 'code',
+      client_id: config.app.spotify.clientId,
+      scope: 'user-read-private user-read-email',
+      redirect_uri: config.app.spotify.clientUri,
+      state: generateRandomString(16),
+    },
+  })
+
+  console.log(res)
+}
 </script>
 
 <template>
-  <NuxtWelcome />
+  <div class="text-center">
+    <button
+      type="button"
+      @click="login"
+      class="spotify-btn"
+    >
+      Login with Spotify
+    </button>
+  </div>
 </template>
+
+<style scoped>
+.text-center {
+  text-align: center;
+}
+
+.spotify-btn {
+  background-color: green;
+  color: white;
+  font-size: 1.8rem;
+  border: none;
+  border-radius: 1rem;
+  padding: 0.5rem 1.6rem;
+  cursor: pointer;
+}
+
+.spotify-btn:hover {
+  opacity: 0.9;
+}
+
+</style>
